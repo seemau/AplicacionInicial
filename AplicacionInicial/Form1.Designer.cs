865fe39de911b3299d512b27656c803091e706c9
seemau <dgp.91@hotmail.com>
2013-10-01 23:14:52 -0600

    Prueba de mi primer repositorio

---
+++ AplicacionInicial/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AplicacionInicial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.RadListDataItem radListDataItem1 = new Telerik.WinControls.UI.RadListDataItem();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.btnInstalar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radDropDownList1 = new Telerik.WinControls.UI.RadDropDownList();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radDropDownList1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AplicacionInicial.Properties.Resources.membreteMain;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnManual
+            // 
+            this.btnManual.Image = global::AplicacionInicial.Properties.Resources.Help_48x48_32__2_;
+            this.btnManual.Location = new System.Drawing.Point(30, 151);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(125, 68);
+            this.btnManual.TabIndex = 4;
+            this.btnManual.Text = "Manual Técnico";
+            this.btnManual.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnManual.UseVisualStyleBackColor = true;
+            this.btnManual.Click += new System.EventHandler(this.btnManual_Click);
+            // 
+            // btnInstalar
+            // 
+            this.btnInstalar.Image = global::AplicacionInicial.Properties.Resources.Misc_Stuff_48x48_32;
+            this.btnInstalar.Location = new System.Drawing.Point(250, 151);
+            this.btnInstalar.Name = "btnInstalar";
+            this.btnInstalar.Size = new System.Drawing.Size(125, 68);
+            this.btnInstalar.TabIndex = 3;
+            this.btnInstalar.Text = "Instalar";
+            this.btnInstalar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnInstalar.UseVisualStyleBackColor = true;
+            this.btnInstalar.Click += new System.EventHandler(this.btnInstalar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "Text";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "saul",
+            "roger",
+            "matamoros"});
+            this.comboBox1.Location = new System.Drawing.Point(133, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.ValueMember = "Value";
+            // 
+            // radDropDownList1
+            // 
+            this.radDropDownList1.DropDownAnimationEnabled = true;
+            radListDataItem1.Tag = "huye";
+            radListDataItem1.Text = "hola";
+            radListDataItem1.TextWrap = true;
+            this.radDropDownList1.Items.Add(radListDataItem1);
+            this.radDropDownList1.Location = new System.Drawing.Point(181, 183);
+            this.radDropDownList1.Name = "radDropDownList1";
+            this.radDropDownList1.ShowImageInEditorArea = true;
+            this.radDropDownList1.Size = new System.Drawing.Size(106, 20);
+            this.radDropDownList1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 233);
+            this.Controls.Add(this.radDropDownList1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.btnInstalar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Instalación SITP";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radDropDownList1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Button btnInstalar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Telerik.WinControls.UI.RadDropDownList radDropDownList1;
+    }
+}
+
---
+++ AplicacionInicial/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AplicacionInicial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInstalar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //System.Diagnostics.Process.Start(@"instalador\setup.exe");
+                //radDropDownList1.SelectedItem.
+                var lista = comboBox1.DisplayMember;
+               
+                this.Close();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(this, Ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnManual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(@"Manual.chm");
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(this, Ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
